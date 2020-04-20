ae35c309f1f89df466924706318b00e7460bc440
Nicolas ENZWEILER <nicolas.enzweiler@gmail.com>
2020-04-20 08:57:44 +0200

    Draft

---
+++ JSONAsyncStorage.js
@@ -0,0 +1,64 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState, useEffect } from "react";
+
+const undefinedAsyncProvider = async () => undefined;
+
+const createRecord = (content, date = new Date()) => ({
+    content,
+    date
+});
+
+const importRecord = data => {
+    if (!data) {
+        return undefined;
+    }
+
+    if (data.date) {
+        return {
+            ...data,
+            date: new Date(data.date)
+        };
+    }
+
+    return createRecord(data);
+};
+
+const JSONAsyncStorage = (
+    key,
+    defaultProvider = undefinedAsyncProvider
+) => ({
+    get: async () => {
+        const record = await AsyncStorage.getItem(key);
+        return record
+            ? importRecord(JSON.parse(record)).content
+            : defaultProvider();
+    },
+    set: async (content) => {
+        const record = createRecord(content);
+        await AsyncStorage.setItem(key, JSON.stringify(record));
+        return record.content;
+    },
+    key: () => key
+});
+
+export const useStorage = (key, defaultProvider) => {
+
+    const storage = new JSONAsyncStorage(key, defaultProvider)
+
+    const [state, setState] = useState(defaultProvider);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        async function fetch() {
+            setState(await storage.get());
+            setLoaded(true);
+        }
+        fetch();
+    }, []);
+
+    async function set(newState) {
+        setState(await storage.set(newState));
+    }
+
+    return [state, set, loaded];
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# ezwn-storage-native
+
+A react-native library providing a hook (useStorage) to store data to ASyncStorage (mobile phone or browser local storage).
+
+## Usage
+
+[ezw-shoppinglist](https://github.com/ezwn/ezwn-shoppinglist) provides an example of usage.
+
+## The author
+
+This project has been developed by Nicolas Enzweiler.
+
+E-Mail address: [nicolas.enzweiler@gmail.com](mailto:nicolas.enzweiler@gmail.com)
+
+Github : [https://github.com/ezwn](https://github.com/ezwn)
